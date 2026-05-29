634cfaab48f628384ee38fc9c8c937780ca5f8d1
zillur03 <magicai@comfythings.com>
2026-05-29 14:11:32 +0600

    Initial commit

--- src/hooks/useLockScreenControls.ts
+++ src/hooks/useLockScreenControls.ts
@@ -17,7 +17,7 @@ export const useLockScreenControls = () => {
 			duration: duration || currentTrack.duration || 0,
 			currentTime: currentTime || 0,
 		});
-	}, [currentTrack, duration, isPlaying]);
+	}, [currentTrack, isPlaying, duration]);
 
 	// Register OS-level lock screen commands (play, pause, skip buttons)
 	useEffect(() => {
